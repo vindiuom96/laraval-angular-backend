191753bc1e7b800e3b3197c340cda4c3a061cfd3
vindiuom96 <vindipererauom@gmail.com>
2018-11-28 21:08:19 +0530

    yhhh

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+Route::group([
+    
+        'middleware' => 'api'
+        
+    
+    ], function ($router) {
+    
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    
+    });
